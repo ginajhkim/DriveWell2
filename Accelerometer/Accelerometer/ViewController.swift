a926cd3cce7b9d9ecf511534c488098f729f45b2
Sherry <sherryxrg@outlook.com>
2019-11-09 20:25:48 -0800

    acceleration counter now working

--- Accelerometer/Accelerometer/ViewController.swift
+++ Accelerometer/Accelerometer/ViewController.swift
@@ -10,50 +10,58 @@ import UIKit
 import CoreMotion
 
 
-
 class ViewController: UIViewController {
-    
     
     @IBOutlet weak var xAccel: UITextField!
     @IBOutlet weak var yAccel: UITextField!
     @IBOutlet weak var zAccel: UITextField!
+    @IBOutlet weak var counterDisplay: UITextField!
+    @IBOutlet weak var counterLabel: UITextField!
     
     var motion = CMMotionManager()
-    
+    var counter = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         myAccelerometer()
     }
+    
     func myAccelerometer() {
-        motion.accelerometerUpdateInterval = 0.5
+        motion.accelerometerUpdateInterval = 0.2
         motion.startAccelerometerUpdates(to: OperationQueue.current!) { (data, error) in print(data as Any)
             if let trueData = data {
                 self.view.reloadInputViews()
                 let x = trueData.acceleration.x
                 let y = trueData.acceleration.y
                 let z = trueData.acceleration.z
+                
+                
                 self.xAccel!.text = "x: \(Double(x).rounded(toPlaces: 3))"
-                self.yAccel!.text = "x: \(Double(y).rounded(toPlaces: 3))"
-                self.zAccel!.text = "x: \(Double(z).rounded(toPlaces: 3))"
+                self.yAccel!.text = "y: \(Double(y).rounded(toPlaces: 3))"
+                self.zAccel!.text = "z: \(Double(z).rounded(toPlaces: 3))"
+                self.counterDisplay!.text = "\(self.counter)"
+                
+                if trueData.acceleration.z > 1 {
+                    self.view.backgroundColor = UIColor.red
+                    self.counter += 1
+                } else {
+                    self.view.backgroundColor = UIColor.white
+                }
             }
-            
         }
+        
         return
     }
     
-    
-
 }
-
-extension Double {
-    func rounded(toPlaces places:Int) -> Double {
-          let divisor = pow(10.0, Double(places))
-          return (self * divisor).rounded() / divisor
+    
+// declaration outside of class
+    extension Double {
+        func rounded(toPlaces places:Int) -> Double {
+              let divisor = pow(10.0, Double(places))
+              return (self * divisor).rounded() / divisor
+        }
     }
-    
 
-    
-}
 
 
